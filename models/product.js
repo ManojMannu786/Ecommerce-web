c8af803cb8fc6f8b3cbf513d7b4d48f1166bec77
Manoj Kumar <mk161098@gmail.com>
2021-10-16 12:08:17 +0530

    edit button funcnality

--- models/product.js
+++ models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id ,title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,12 +27,22 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      if (this.id){
+        const existingProductIndex = products.findIndex(prod => prod.id === this.id)
+        const updatedProductes = [...products];
+        updatedProductes[existingProductIndex]=this;
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
+      else{
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
     });
   }
 
@@ -39,10 +50,10 @@ module.exports = class Product {
     getProductsFromFile(cb);
   }
 
-  static findById (id,cb){
-    getProductsFromFile(products=>{
-      const ourProduct = products.find(p=> p.id === id)
-      cb(ourProduct)
-    })
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
   }
 };
