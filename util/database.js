272593aab3abccada5fba03afac696681c529dc1
Manoj Kumar <mk161098@gmail.com>
2021-10-18 16:01:28 +0530

    sequelize initialise

--- util/database.js
+++ util/database.js
@@ -1,10 +1,19 @@
-const database = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = database.createPool({
-    host : 'localhost',
-    user: 'root',
-    database : 'node-complete',
-    password : '1234'
+const sequelize = new Sequelize('node-complete', 'root', '1234', {
+    dialect: 'mysql',
+    host: 'localhost'
 })
 
-module.exports = pool.promise();
+module.exports = sequelize;
+
+// const database = require('mysql2');
+
+// const pool = database.createPool({
+//     host : 'localhost',
+//     user: 'root',
+//     database : 'node-complete',
+//     password : '1234'
+// })
+
+// module.exports = pool.promise();
